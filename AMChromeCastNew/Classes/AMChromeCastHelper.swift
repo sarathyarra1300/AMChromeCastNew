64c2d73b245f337c9d5cce7a44e6560b9adfcfcc
sarathyarra1300 <sarath.yarra1300@gmail.com>
2019-08-06 12:28:16 +0530

    added AMChromeCastHelper.swift and removed ReplaceMe.swift

---
+++ AMChromeCastNew/Classes/AMChromeCastHelper.swift
@@ -0,0 +1,18 @@
+//
+//  AMChromeCastHelper.swift
+//  AMChromeCastNew
+//
+//  Created by Sarath Yarra on 06/08/19.
+//
+
+import UIKit
+
+public class AMChromeCastHelper: NSObject {
+    
+    public  static let sharedInstance: AMChromeCastHelper = { AMChromeCastHelper() }()
+   public func InitilizeChromeCast ()
+   {
+    
+    }
+
+}
