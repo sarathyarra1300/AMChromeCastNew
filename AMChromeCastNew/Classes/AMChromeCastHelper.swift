a40f160c1f9836b5bca60cb68eaeed5c9a3b4d13
sarathyarra1300 <sarath.yarra1300@gmail.com>
2019-08-06 20:13:44 +0530

    Install pod files for chrome cast

--- AMChromeCastNew/Classes/AMChromeCastHelper.swift
+++ AMChromeCastNew/Classes/AMChromeCastHelper.swift
@@ -6,13 +6,33 @@
 //
 
 import UIKit
+import GoogleCast
 
-public class AMChromeCastHelper: NSObject {
-    
+
+
+@objc public class AMChromeCastHelper: NSObject,GCKLoggerDelegate,GCKSessionManagerListener,GCKDiscoveryManagerListener,GCKRequestDelegate {
+    @objc public let kReceiverAppID = ""
+    var _sessionManager : GCKSessionManager?
+    var _castMediaController: GCKUIMediaController?
     public  static let sharedInstance: AMChromeCastHelper = { AMChromeCastHelper() }()
-   public func InitilizeChromeCast ()
-   {
     
+    
+    // Initilize the chrome cast
+    @objc  public func InitilizeChromeCast ()
+    {
+        let criteria = GCKDiscoveryCriteria(applicationID: kReceiverAppID)
+        let options = GCKCastOptions.init(discoveryCriteria: criteria)
+        GCKCastContext.setSharedInstanceWith(options)
+        _castMediaController = GCKUIMediaController()
+        
+        // Enable logger.
+        GCKLogger.sharedInstance().delegate = self
+        
+        GCKCastContext.sharedInstance().useDefaultExpandedMediaControls = true
+        
+        _sessionManager = GCKCastContext.sharedInstance().sessionManager
+        _sessionManager!.add(self)
+        print("google chrome cast initilization")
     }
-
+    
 }
